d243bd2cd88ca5a6706773f0ad477c78f6e36b88
manu <manu.villar.vitoria@gmail.com>
2020-05-24 17:40:20 +0200

    creating a button with his 'onClick' event and installing 'Emotion'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React,{Fragment} from 'react';
+//Instalamos emotion (npm i @emotion/core @emotion/styled) y lo importamos:
+import styled from '@emotion/styled';
 
+const Contenedor = styled.div `
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
 
+const Boton = styled.button `
+  background: -webkit-linear-gradient(top left, #007d35 0%,  #007d35 40%,  #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const consultarAPI = () => {
+    console.log('consultando...');
+  }
+
   return (
     <Fragment>
-      <h1>Breaking Bad</h1>
+      <Contenedor>
+        <Boton
+          //Si se quiere que la función llame automaticamente sin esperar a clickar el 
+          // Botón entonces se debería llamar así: consultarAPI()
+          onClick={consultarAPI} //Es lo mismo que hacer esto: () => consultarAPI() 
+        >
+          Obtener Frase
+        </Boton>
+      </Contenedor>
     </Fragment>
   );
 }
